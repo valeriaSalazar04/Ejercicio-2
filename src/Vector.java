a612d7f6ec96e69b18fce99b6aa177a068c7c18c
ValeriaG <ValeriaG@ValeriaG-PC>
2014-01-20 23:30:57 -0500

    Método unir Vectores

--- src/Vector.java
+++ src/Vector.java
@@ -2,12 +2,64 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
+/**
+ * Inicialización de la clase Vector
+ */
 /**
  *
  * @author ValeriaG
  */
 public class Vector {
     
+      private int vector[];
+    
+      /**
+       * Constructor vacío de la clase vector
+       */
+      public Vector (){
+  } 
+      /**
+       * 
+       * @param tamaño 
+       */
+      public Vector (int tamaño){
+          
+         this.vector= new int [tamaño];
+        
+      }
+     /**
+      * 
+      * @param i
+      * @param dato 
+      */
+     public void set(int i, int dato){
+      
+        this.vector[i]=dato;
+    }
+     /**
+      * 
+      * @return 
+      */
+      public int cardinalidad(){
+    
+        return this.vector.length;
+    
+    }
+       public String toString(){
+    
+        String msg="";
+        for(int dato:this.vector)
+            msg+=dato+",";
+        return(msg);
+    }
+       
+       public int get(int i){
+       
+        return this.vector[i];
+    }
+      
+       
+         
+       
     
 }
--- src/testVector.java
+++ src/testVector.java
@@ -10,28 +10,56 @@
 public class testVector {
      public static void main(String nada[]){
     
-    
-    Vector discretas= new Vector (4);
-    Vector programacion = new Vector (3);
-    Vector fisica = new Vector (2);
-    
+         Vector discretas = new Vector (3);
+          Vector fisica = new Vector (4);
+           Vector programacion = new Vector (3);
+     
     llenarVector(discretas);
     llenarVector(fisica);
+    llenarVector(programacion);
     imprimir(discretas);
     imprimir(fisica);
-} 
-    public static void llenarVector (Vector v[]){
+    imprimir(programacion);
+    Vector n = unirVector(discretas, fisica);
+    imprimir(n);
+    
+}
+      
+    
+    public static void llenarVector (Vector v){
         
-         for(int i=0;i<v.length;i++)
+         for(int i=0;i<v.cardinalidad();i++)
         {
-            int dato=(int)(Math.random()*100);
-            v.set
+            int dato=(int)(Math.random()*10);
+            v.set(i, dato);
                     
+            
     }
     }
-   public static void imprimir (int v[]){
+   public static void imprimir (Vector v){
              System.out.println(v.toString());
              
          }
-    
+     public static Vector unirVector (Vector v, Vector v2){
+        
+         int tamaño = v.cardinalidad()+v2.cardinalidad();
+         Vector nuevo=new Vector (tamaño);
+         
+        int d=tamaño-v2.cardinalidad();
+         for (int i=0;i<v.cardinalidad();i++){
+                int dato= v.get(i);
+                nuevo.set(++d,dato);
+              for (int j=0;j<v2.cardinalidad();j++){
+                   int dato2= v2.get(j);
+                 nuevo.set(j,dato2);
+             
+//             
+             
+              }       
+         }
+         
+          return nuevo;
+
+
+     }
 }
