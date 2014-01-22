a0590de0c4cc72cb13f9ce9d32e55e4063b5b82a
ValeriaG <ValeriaG@ValeriaG-PC>
2014-01-22 13:49:48 -0500

    Método eliminarRepetidos

--- src/testVector.java
+++ src/testVector.java
@@ -29,8 +29,10 @@ public class testVector {
         Vector n = unirVector(discretas, fisica);
         imprimir(n);
         Vector m = unirVector(fisica, programacion);
-       
+        
         imprimir(m);
+        
+        
         
       
     }
@@ -69,60 +71,57 @@ public class testVector {
 
         int tamaño = v.cardinalidad() + v2.cardinalidad();
         Vector nuevo = new Vector(tamaño);
-        int i ;
-        
-       
-       
-        
-        for ( i=0;i<v.cardinalidad();i++){
-           
-            int da= v.get(i);
-            nuevo.set(i,v.get(i));
+        int i;
+
+        for (i = 0; i < v.cardinalidad(); i++) {
+
+            int da = v.get(i);
+            nuevo.set(i, v.get(i));
         }
-            
-            for (i=0;i<v2.cardinalidad();i++){
-               
-                int da2=v2.get(i);
-                nuevo.set(v.cardinalidad()+i,da2);
-            
-           
-            }
-        return nuevo;
+
+        for (i = 0; i < v2.cardinalidad(); i++) {
+
+            int da2 = v2.get(i);
+            nuevo.set(v.cardinalidad() + i, da2);
+
+
+        }
+        Vector c= eliminarRepetidos(nuevo);
+        
+        return c;
     }
-}
-//        for ( int i=0;i < v.cardinalidad(); i++) {
-//            
-//            int dato = v.get(i);
-//            nuevo.set(j++, dato);
-//            for (; j < v2.cardinalidad(); j++) {
-//                int dato2 = v2.get(j);
-//                nuevo.set(j, dato2);
-//               
-//            
-//
-//            }
-//        }
-//         
-////        Vector comparados=comparar(nuevo);
-//        return nuevo;
-//
+
 
     
+    public static Vector eliminarRepetidos (Vector v){
+        int j;
+        int cont=0;
+        Vector nuevo = new Vector(v.cardinalidad());
+       
+       for (int i =0;i<v.cardinalidad();i++){
+           int elemento =v.get(i);
+           boolean elementoRepetido=false;
+           
+           for ( j=0;j<nuevo.cardinalidad();j++){
+               if (nuevo.get(j)==elemento){
+                   elementoRepetido=true;
+                   cont ++;
+                   break;
+               }
+           }
+               if (elementoRepetido==false){
+                   int tam= nuevo.cardinalidad()-cont;
+                   Vector temporal =new Vector (nuevo.cardinalidad()+1);
+                   for( j = 0; j<nuevo.cardinalidad(); j++){ 
+                      temporal.set(j, nuevo.get(j));
+               }
+                   temporal.set(j, elemento);
+                   nuevo = temporal;
+           }
+       }
+      
     
-//    public static Vector comparar (Vector v){
-//        
-//        Vector nuevo = new Vector(v.cardinalidad());
-//        int i=0, j=i+1;
-//        int dato = v.get(i);
-//        int dato2 = nuevo.get(j);
-//        boolean repetido;
-//        for (i = 0; i < v.cardinalidad(); i++) {
-//         if (v.get(i)==v.get(j)){
-//             nuevo.set(i, i);
-//             
-//         }
-//        }   
-//        
-//       return nuevo;
-//    }
-//
+       return nuevo;
+}
+    
+}
